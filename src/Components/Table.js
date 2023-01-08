f6c42ec7b1eb39dcad8b8a9d7ca630f799bee0d9
Ahmed Ali <90496364+chAhmii@users.noreply.github.com>
2023-01-08 09:26:56 -0800

    Blockchain implementation

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -6,71 +6,55 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-const locations = [
-    {
-      //isl
-      city: "Islamabad",
-      longitude: 73.047882,
-      latitude: 33.684422,
-      day: "Monday",
-      time: "12:35:10",
-    },
-    {
-      //london
-      city: "Lahore",
-      longitude: 0.127758,
-      latitude: 51.507351,
-      day: "Tuesday",
-      time: "01:35:10",
-    },
-    {
-      //london
-      city: "Lahore",
-      longitude: 0.127758,
-      latitude: 51.507351,
-      day: "Monday",
-      time: "01:35:10",
-    },
-    {
-      //london
-      city: "Lahore",
-      longitude: 0.127758,
-      latitude: 51.507351,
-      day: "Thursday",
-      time: "06:35:10",
-    },
-    {
-      //Karachi
-      city: "Karachi",
-      longitude: 0.127758,
-      latitude: 51.507351,
-      day: "Friday",
-      time: "03:15:40",
-    },
-    {
-      //london
-      city: "Karachi",
-      longitude: 0.127858,
-      latitude: 51.501351,
-      day: "Monday",
-      time: "02:11:12",
-    },
-  ];
+import Web3 from "web3";
+import { useState, useEffect} from "react";
+import { loadContract } from "../utils/load-contract";
 
-
-function createData(city, longitude, latitude, day, time) {
-  return { city, longitude, latitude, day, time };
+function createData(city, longitude, latitude, day) {
+  return { city, longitude, latitude, day};
 }
 
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
 export default function BasicTable() {
+  const [web3Api, setWeb3Api] = useState({
+    contract: null,
+  });
+
+  const [locations, setData] = useState([]);
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      const contract = await loadContract("Trash", "HTTP://127.0.0.1:7545");
+      setWeb3Api({
+        contract,
+      });
+    };
+    loadProvider();
+  }, []);
+
+  async function show_locations(val) {
+    const dict = { longitude: "", latitude: "", city: "" ,day:"",time:""};
+    console.log("epiahdns", val.longitude);
+    dict["longitude"] = val.longitude;
+    dict["latitude"] = val.latitude;
+    dict["city"] = val.city;
+    dict["day"] = val.day;
+    setData(locations => [...locations, dict]);
+  }
+
+  
+
+  useEffect(() => {
+    const { contract } = web3Api;
+    const get_solved = async () => {
+      let id_var = null;
+      await contract.Get_locations_resolved().then(function (resp) {
+        id_var = resp;
+      });
+      id_var.map(show_locations);
+    };
+    contract && get_solved(contract);
+  }, [web3Api]);
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -88,9 +72,6 @@ export default function BasicTable() {
             <TableCell align="center" style={{ color: "white" }}>
               Day
             </TableCell>
-            <TableCell align="center" style={{ color: "white" }}>
-              Time
-            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -99,13 +80,12 @@ export default function BasicTable() {
               key={location.name}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
-              <TableCell component="th" scope="row" align="center">
+              <TableCell component="th" align="center" scope="row">
                 {location.city}
               </TableCell>
               <TableCell align="center">{location.longitude}</TableCell>
               <TableCell align="center">{location.latitude}</TableCell>
               <TableCell align="center">{location.day}</TableCell>
-              <TableCell align="center">{location.time}</TableCell>
             </TableRow>
           ))}
         </TableBody>
